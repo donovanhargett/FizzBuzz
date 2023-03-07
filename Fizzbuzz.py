21b410dda701b323256f8d4696254494dcdc48e9
donovanhargett <donovanhargett@gmail.com>
2023-03-06 22:15:30 -0600

    Step 1 done

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,3 +1,14 @@
 # FizzBuzz program
+#STEP 1 : Get program to work
+
+for i in range(1, 101): 
+    if i % 3 == 0 and  i % 5 == 0:
+        print("FizzBuzz")
+    elif(i % 3 == 0):
+        print("Fizz")
+    elif(i % 5 ==  0):
+        print("Buzz")
+    else:
+        print(i)
 
 
